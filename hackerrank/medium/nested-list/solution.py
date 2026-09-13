cefbeea156b81f27b0250a05f863b29b70e885bd
Akaza-17 <oshadhaweerakoon1@gmail.com>
2026-09-14 00:01:36 +0530

    ✅ Nested Lists | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-13

--- hackerrank/medium/nested-list/solution.py
+++ hackerrank/medium/nested-list/solution.py
@@ -1,10 +1,20 @@
 if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    
-    def run_up(num,ar):
-        if 2<=num<=10:
-            scores = sorted(set(ar), reverse=True)
-            print(scores[1])
+    students = []
+    marks = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name,score])
+        marks.append(score)
         
-run_up(n,arr)
+def sec_low(students,score):
+    my_set = sorted(set(score))
+    x= my_set[1]
+    results = []
+    for student in students:
+        if student [1]==x:
+            results.append(student[0])
+    for result in sorted(results):
+        print(result)
+                
+sec_low(students,marks)
